2c6ac84d3d9e35cea61d747232f9a7b69c2cd5b9
aerhergag00 <jwlee101500@gmail.com>
2025-04-09 02:13:39 +0900

    fix #13: ReqPaymentPostApproveDtoApiV1 내용 임시 추가

--- payment-service/src/main/java/com/musinsam/paymentservice/application/dto/request/ReqPaymentPostApproveDtoApiV1.java
+++ payment-service/src/main/java/com/musinsam/paymentservice/application/dto/request/ReqPaymentPostApproveDtoApiV1.java
@@ -1,5 +1,6 @@
 package com.musinsam.paymentservice.application.dto.request;
 
+import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,5 +11,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReqPaymentPostApproveDtoApiV1 {
-  // TODO:
+
+  private PaymentApproval paymentApproval;
+
+  /**
+   * 결제 승인 정보에 대한 정보 입니다.
+   */
+  @Getter
+  @Builder
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class PaymentApproval {
+
+    private String paymentKey;
+    private String orderId;
+    private BigDecimal amount;
+    private String responseCode;
+    private String responseMessage;
+    private String metadata;
+  }
 }
