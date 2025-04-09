54d6d08fbe6fc681617061e5fe91bc59c8464382
Sohae Park <ipnsohe@naver.com>
2025-04-10 01:35:33 +0900

    feat #26: Event 관련 DTO 설계 및 상태(Enum) 추가
    - 문자열을 EventStatus로 변환하는 메서드 구현
    - 대소문자 처리를 위한 parsing 메서드 구현

---
+++ event-service/src/main/java/com/musinsam/eventservice/application/dto/response/ResEventGetListDtoApiV1.java
@@ -0,0 +1,51 @@
+package com.musinsam.eventservice.application.dto.response;
+
+import com.musinsam.eventservice.domain.event.EventStatus;
+import com.musinsam.eventservice.domain.event.entity.EventEntity;
+import java.time.ZonedDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResEventGetListDtoApiV1 {
+
+  private Event event;
+
+  public static ResEventGetListDtoApiV1 of(EventEntity eventEntity) {
+    return ResEventGetListDtoApiV1.builder()
+        .event(Event.from(eventEntity))
+        .build();
+  }
+
+  @Getter
+  @Builder
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class Event {
+
+    private UUID id;
+    private String name;
+    private ZonedDateTime startTime;
+    private ZonedDateTime endTime;
+    private EventStatus status;
+
+
+    public static Event from(EventEntity eventEntity) {
+      return Event.builder()
+          .id(null)
+          .name(null)
+          .startTime(null)
+          .endTime(null)
+          .status(null)
+          .build();
+    }
+
+  }
+
+}
---
+++ event-service/src/main/java/com/musinsam/eventservice/domain/event/EventStatus.java
@@ -0,0 +1,21 @@
+package com.musinsam.eventservice.domain.event;
+
+import java.util.stream.Stream;
+
+public enum EventStatus {
+
+  SCHEDULED,
+  ACTIVE,
+  COMPLETED;
+
+  public static EventStatus of(String status) {
+    return valueOf(status);
+  }
+
+  public static EventStatus parsing(String inputValue) {
+    return Stream.of(EventStatus.values())
+        .filter(eventStatus -> eventStatus.toString().equals(inputValue.toUpperCase()))
+        .findFirst()
+        .orElse(null);
+  }
+}
