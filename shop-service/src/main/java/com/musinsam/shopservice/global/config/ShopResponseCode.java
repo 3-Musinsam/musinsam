ffdb551d3c369841a590c302c19fcd9e93da79b8
Sohae Park <ipnsohe@naver.com>
2025-04-10 10:25:01 +0900

    feat #28: Shop Service 응답 코드 정의

---
+++ shop-service/src/main/java/com/musinsam/shopservice/global/config/ShopResponseCode.java
@@ -0,0 +1,21 @@
+package com.musinsam.shopservice.global.config;
+
+import com.musinsam.common.response.SuccessCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+@Getter
+public enum ShopResponseCode implements SuccessCode {
+
+  SHOP_CREATE_SUCCESS(0, "상점 생성 성공", HttpStatus.CREATED),
+  SHOP_GET_LIST_SUCCESS(0, "상점 목록 조회 성공", HttpStatus.OK),
+  SHOP_GET_SUCCESS(0, "상점 상세 조회 성공", HttpStatus.OK),
+  SHOP_UPDATE_SUCCESS(0, "상점 수정 성공", HttpStatus.OK),
+  SHOP_DELETE_SUCCESS(0, "상점 삭제 성공", HttpStatus.OK);
+
+  private final Integer code;
+  private final String message;
+  private final HttpStatus httpStatus;
+}
