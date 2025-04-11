6dbb885c27e2a0d7380746363d83b01b9617925a
LimdaeIl <optional94@naver.com>
2025-04-11 10:44:08 +0900

    refactor #77: redis 구현체명 및 설정 위치 수정
    단순히 redis 를 의미하는 구현을 나타내는 것은 잘못된 선택이다.
    해당 기능의 의미를 명확하게 나타내고, 인프라스트럭처 계층에서 구현체를 구현한다.

--- user-service/src/main/java/com/musinsam/userservice/app/infrastructure/redis/RefreshTokenRepositoryImpl.java
+++ user-service/src/main/java/com/musinsam/userservice/app/infrastructure/redis/RefreshTokenRepositoryImpl.java
@@ -1,19 +1,18 @@
-package com.musinsam.userservice.app.application.service.redis;
+package com.musinsam.userservice.app.infrastructure.redis;
 
+import com.musinsam.userservice.app.domain.user.repository.token.RefreshTokenRepository;
 import java.time.Duration;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBucket;
 import org.redisson.api.RedissonClient;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Getter
 @RequiredArgsConstructor
 @Service
-public class RedisService {
+public class RefreshTokenRepositoryImpl implements RefreshTokenRepository {
 
   private final RedissonClient redissonClient;
 
