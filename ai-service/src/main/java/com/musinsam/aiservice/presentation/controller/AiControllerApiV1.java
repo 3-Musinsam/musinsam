05b51ffe20596b4eea73626571f94037a4964300
adgjl <adgjl0122@naver.com>
2025-04-11 22:53:37 +0900

    feat #86: Ai 도메인 테스트코드 작성
    - valid 작성, application.yml value 수정

--- ai-service/src/main/java/com/musinsam/aiservice/presentation/controller/AiControllerApiV1.java
+++ ai-service/src/main/java/com/musinsam/aiservice/presentation/controller/AiControllerApiV1.java
@@ -3,23 +3,26 @@ package com.musinsam.aiservice.presentation.controller;
 import com.musinsam.aiservice.application.dto.request.ReqAiPostDtoApiV1;
 import com.musinsam.aiservice.application.service.ChatGptService;
 import com.musinsam.common.response.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/ais")
+@RequestMapping("/internal/v1/ai-message")
 public class AiControllerApiV1 {
 
   private final ChatGptService chatGptService;
 
   @PostMapping
-  public ResponseEntity<ApiResponse<String>> postBy(@RequestBody ReqAiPostDtoApiV1 dto) {
+  public ResponseEntity<ApiResponse<String>> postBy(@Valid @RequestBody ReqAiPostDtoApiV1 dto) {
     String message = chatGptService.completeChat(dto);
 
     return new ResponseEntity<>(
