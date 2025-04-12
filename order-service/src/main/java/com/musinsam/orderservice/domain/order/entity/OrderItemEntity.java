02bb7538e2583bb297e59f63989c186a71672d40
aerhergag00 <jwlee101500@gmail.com>
2025-04-12 16:07:30 +0900

    feat #64: 주문 엔티티

--- order-service/src/main/java/com/musinsam/orderservice/domain/order/entity/OrderItemEntity.java
+++ order-service/src/main/java/com/musinsam/orderservice/domain/order/entity/OrderItemEntity.java
@@ -1,8 +1,8 @@
 package com.musinsam.orderservice.domain.order.entity;
 
 import com.musinsam.common.domain.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -29,21 +29,29 @@ public class OrderItemEntity extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "id")
   private UUID id;
 
+  @Column(name = "product_id")
   private UUID productId;
 
+  @Column(name = "product_name")
   private String productName;
 
+  @Column(name = "price")
   private BigDecimal price;
 
+  @Column(name = "quantity")
   private Integer quantity;
 
+  @Column(name = "discount_amount")
   private BigDecimal discountAmount;
 
+  @Column(name = "final_amount")
   private BigDecimal finalAmount;
 
-  @ManyToOne(fetch = FetchType.LAZY)
+  @ManyToOne
   @JoinColumn(name = "order_id")
   private OrderEntity order;
+
 }
