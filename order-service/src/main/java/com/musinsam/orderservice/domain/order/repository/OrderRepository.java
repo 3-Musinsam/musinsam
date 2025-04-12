0cd3daa99d4005794d60bd4a9ff97617bb416dab
aerhergag00 <jwlee101500@gmail.com>
2025-04-12 16:08:16 +0900

    feat #64: 주문 생성

--- order-service/src/main/java/com/musinsam/orderservice/domain/order/repository/OrderRepository.java
+++ order-service/src/main/java/com/musinsam/orderservice/domain/order/repository/OrderRepository.java
@@ -1,5 +1,8 @@
 package com.musinsam.orderservice.domain.order.repository;
 
+import com.musinsam.orderservice.domain.order.entity.OrderEntity;
+
 public interface OrderRepository {
 
+  OrderEntity save(OrderEntity orderEntity);
 }
