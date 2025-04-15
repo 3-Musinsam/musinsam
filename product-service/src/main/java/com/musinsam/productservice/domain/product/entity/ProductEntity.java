6fb6219caaea5a22b7951f0b06d6e8d4c20b6436
Sohae Park <ipnsohe@naver.com>
2025-04-15 11:55:37 +0900

    feat #103: 이벤트 관련 엔티티 구현
    - ProductEntity
    - ProductImageEntity
    - ProductCouponEntity

--- product-service/src/main/java/com/musinsam/productservice/domain/product/entity/ProductEntity.java
+++ product-service/src/main/java/com/musinsam/productservice/domain/product/entity/ProductEntity.java
@@ -1,8 +1,54 @@
 package com.musinsam.productservice.domain.product.entity;
 
+import com.musinsam.common.domain.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
 
+@Entity
+@Table(name = "p_product")
+@Getter
 @Builder
-public class ProductEntity {
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductEntity extends BaseEntity {
+
+  @Id
+  @UuidGenerator
+  private UUID id;
+
+  @Column(nullable = false)
+  private UUID shopId;
+
+  @Setter
+  @Column(nullable = false)
+  private String name;
+
+  @Setter
+  @Column(nullable = false)
+  private BigDecimal price;
+
+  @Column
+  private BigDecimal discountPrice;
+
+  @Setter
+  @Column(nullable = false)
+  private Integer stock;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private ProductStatus status = ProductStatus.SHOW;
 
 }
---
+++ product-service/src/main/java/com/musinsam/productservice/domain/product/entity/ProductStatus.java
@@ -0,0 +1,22 @@
+package com.musinsam.productservice.domain.product.entity;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.stream.Stream;
+
+public enum ProductStatus {
+  SHOW,
+  SOLDOUT,
+  HIDE;
+
+  public static ProductStatus of(String status) {
+    return valueOf(status);
+  }
+
+  @JsonCreator
+  public static ProductStatus parsing(String inputValue) {
+    return Stream.of(ProductStatus.values())
+        .filter(productStatus -> productStatus.toString().equals(inputValue.toUpperCase()))
+        .findFirst()
+        .orElse(null);
+  }
+}
