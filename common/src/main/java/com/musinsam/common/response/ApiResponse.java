6979328a02a88975c50de8d8da67f82ebb2e46ce
LimdaeIl <optional94@naver.com>
2025-04-15 18:30:30 +0900

    refactor #119: NoArgsContructor 제거

--- common/src/main/java/com/musinsam/common/response/ApiResponse.java
+++ common/src/main/java/com/musinsam/common/response/ApiResponse.java
@@ -5,10 +5,7 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
-@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Builder
