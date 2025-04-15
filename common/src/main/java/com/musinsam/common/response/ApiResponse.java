656938d673c98b6e8ba04f06623d043215083a11
Sohae Park <ipnsohe@naver.com>
2025-04-15 22:08:45 +0900

    fix #105 : ApiResponse @NoArgsConstructor 어노테이션 삭제

--- common/src/main/java/com/musinsam/common/response/ApiResponse.java
+++ common/src/main/java/com/musinsam/common/response/ApiResponse.java
@@ -5,10 +5,8 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
-@NoArgsConstructor
+//@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Builder
