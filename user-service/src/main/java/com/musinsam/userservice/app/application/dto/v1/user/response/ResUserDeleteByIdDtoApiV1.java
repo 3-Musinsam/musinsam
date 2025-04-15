e02dbaa3ed98cdc4e8cd7c9fd4fe219aa6a77588
LimdaeIl <optional94@naver.com>
2025-04-15 15:07:38 +0900

    feat #113: delete 시 dto 응답 구현
    void 가 아니라 삭제한 계정 정보를 반환하도록 수정

--- user-service/src/main/java/com/musinsam/userservice/app/application/dto/v1/user/response/ResUserDeleteByIdDtoApiV1.java
+++ user-service/src/main/java/com/musinsam/userservice/app/application/dto/v1/user/response/ResUserDeleteByIdDtoApiV1.java
@@ -1,5 +1,60 @@
 package com.musinsam.userservice.app.application.dto.v1.user.response;
 
+import com.musinsam.userservice.app.application.dto.v1.user.response.ResUserDeleteByIdDtoApiV1.User;
+import com.musinsam.userservice.app.domain.user.entity.UserEntity;
+import com.musinsam.userservice.app.domain.user.vo.UserRoleType;
+import java.time.ZonedDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ResUserDeleteByIdDtoApiV1 {
-  // TODO: 단순 응답으로 처리할 지 고민
+
+  private User user;
+
+  public static ResUserDeleteByIdDtoApiV1 of(UserEntity userEntity) {
+    return ResUserDeleteByIdDtoApiV1.builder()
+        .user(User.from(userEntity))
+        .build();
+  }
+
+  @Getter
+  @Builder
+  @NoArgsConstructor(access = AccessLevel.PRIVATE)
+  @AllArgsConstructor(access = AccessLevel.PRIVATE)
+  public static class User {
+    private Long id;
+    private String email;
+    private String name;
+    private UserRoleType role;
+    private ZonedDateTime createdAt;
+    private Long createdBy;
+    private ZonedDateTime updatedAt;
+    private Long updatedBy;
+    private ZonedDateTime deletedAt;
+    private Long deletedBy;
+
+
+    public static User from(UserEntity userEntity) {
+      return User.builder()
+          .id(userEntity.getId())
+          .email(userEntity.getEmail())
+          .name(userEntity.getName())
+          .role(userEntity.getUserRoleType())
+          .createdAt(userEntity.getCreatedAt())
+          .createdBy(userEntity.getCreatedBy())
+          .updatedAt(userEntity.getUpdatedAt())
+          .updatedBy(userEntity.getUpdatedBy())
+          .deletedAt(userEntity.getDeletedAt())
+          .deletedBy(userEntity.getDeletedBy())
+          .build();
+    }
+  }
 }
