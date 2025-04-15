99600ad06e6454edb4b653a735aa5459b2c7eb5b
LimdaeIl <optional94@naver.com>
2025-04-15 23:04:00 +0900

    feat #125: 단 건 쿠폰 정책 서비스 구현 및 파라미터 수정
    findCouponPolicyById에서 Long -> UUID 올바르게 수정

--- coupon-service/src/main/java/com/musinsam/couponservice/app/application/service/couponPolicy/CouponPolicyService.java
+++ coupon-service/src/main/java/com/musinsam/couponservice/app/application/service/couponPolicy/CouponPolicyService.java
@@ -2,8 +2,12 @@ package com.musinsam.couponservice.app.application.service.couponPolicy;
 
 import com.musinsam.common.user.CurrentUserDtoApiV1;
 import com.musinsam.couponservice.app.application.dto.v1.couponPolicy.request.ReqCouponPolicyIssueDtoApiV1;
+import com.musinsam.couponservice.app.application.dto.v1.couponPolicy.response.ResCouponPolicyGetDtoApiV1;
 import com.musinsam.couponservice.app.application.dto.v1.couponPolicy.response.ResCouponPolicyIssueDtoApiV1;
+import java.util.UUID;
 
 public interface CouponPolicyService {
   ResCouponPolicyIssueDtoApiV1 issueCouponPolicy(ReqCouponPolicyIssueDtoApiV1 request, CurrentUserDtoApiV1 currentUser);
+
+  ResCouponPolicyGetDtoApiV1 getCouponPolicy(UUID id, CurrentUserDtoApiV1 currentUser);
 }
