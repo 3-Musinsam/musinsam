a47fc465fbb6d56734221c8e52b130f056e92f3f
aerhergag00 <jwlee101500@gmail.com>
2025-04-17 10:50:10 +0900

    fix #153: application.yml 및 TossPaymentFeignClient 코드 수정

--- payment-service/src/main/java/com/musinsam/paymentservice/infrastructure/client/TossPaymentFeignClient.java
+++ payment-service/src/main/java/com/musinsam/paymentservice/infrastructure/client/TossPaymentFeignClient.java
@@ -8,10 +8,10 @@ import java.math.BigDecimal;
 import java.util.Map;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
     name = "toss-payment",
@@ -32,13 +32,13 @@ public interface TossPaymentFeignClient {
   @GetMapping("/v1/payments/{paymentKey}")
   Map<String, Object> getByPaymentKey(
       @RequestHeader("Authorization") String authorization,
-      @RequestParam("paymentKey") String paymentKey
+      @PathVariable("paymentKey") String paymentKey
   );
 
   @GetMapping("/v1/payments/orders/{orderId}")
   Map<String, Object> getByOrderId(
       @RequestHeader("Authorization") String authorization,
-      @RequestParam("orderId") String orderId
+      @PathVariable("orderId") String orderId
   );
 
   /**
