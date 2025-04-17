ac53c9553aa615b7cfa189ecf59b3728da10a852
Sohae Park <ipnsohe@naver.com>
2025-04-18 01:08:20 +0900

    fix #172 : FileService를 ProductService에 통합

--- product-service/src/main/java/com/musinsam/productservice/application/service/impl/ProductServiceImplApiV1.java
+++ product-service/src/main/java/com/musinsam/productservice/application/service/impl/ProductServiceImplApiV1.java
@@ -15,13 +15,15 @@ import com.musinsam.productservice.domain.product.entity.ProductImageEntity;
 import com.musinsam.productservice.domain.product.repository.ProductImageRepository;
 import com.musinsam.productservice.domain.product.repository.ProductRepository;
 import com.musinsam.productservice.infrastructure.dto.res.ResShopCouponDto;
-import com.musinsam.productservice.infrastructure.image.S3Folder;
-import com.musinsam.productservice.infrastructure.image.service.FileService;
-import com.musinsam.productservice.infrastructure.image.service.S3Service;
+import com.musinsam.productservice.infrastructure.s3.S3Folder;
+import com.musinsam.productservice.infrastructure.s3.service.S3Service;
+import io.micrometer.common.util.StringUtils;
 import java.time.ZoneId;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -34,7 +36,6 @@ public class ProductServiceImplApiV1 implements ProductServiceApiV1 {
 
   private final ProductRepository productRepository;
   private final ProductImageRepository productImageRepository;
-  private final FileService fileService;
   private final S3Service s3Service;
 
   @Override
@@ -46,7 +47,7 @@ public class ProductServiceImplApiV1 implements ProductServiceApiV1 {
     productRepository.save(product);
 
     for (MultipartFile image : images) {
-      fileService.saveImageFile(S3Folder.PRODUCT, image, product.getId());
+      saveImageFile(S3Folder.PRODUCT, image, product.getId());
     }
   }
 
@@ -167,5 +168,60 @@ public class ProductServiceImplApiV1 implements ProductServiceApiV1 {
 
       // 다르면 예외
     }
+  }
+
+
+  @Value("${file.image-extension}")
+  private String imageExtension;
+
+
+  @Override
+  @Transactional
+  public void saveImageFile(S3Folder s3Folder, MultipartFile file, UUID id) {
+    // S3 폴더 파라미터 확인
+    if (s3Folder == null) {
+      throw new RuntimeException();
+    }
+
+    // 파일이 비어있는지 확인
+    if (file == null || file.isEmpty()) {
+      throw new RuntimeException();
+    }
+
+    // 파일 확장자 확인
+    final String fileExtension = FilenameUtils.getExtension(file.getOriginalFilename());
+    if (StringUtils.isBlank(fileExtension) || !imageExtension.contains(fileExtension)) {
+      throw new RuntimeException();
+    }
+
+    // 파일 이름 생성
+    final String fileName = UUID.randomUUID() + "." + fileExtension;
+
+    // S3에 업로드
+    s3Service.uploadFile(fileName, s3Folder, file);
+
+    if (s3Folder == S3Folder.PRODUCT) {
+      saveProductImageInfo(s3Folder, file, id, fileName);
+    }
+  }
+
+  @Override
+  @Transactional
+  public void saveProductImageInfo(S3Folder s3Folder, MultipartFile file, UUID id,
+      String fileName) {
+
+    ProductEntity product = productRepository.findByIdAndDeletedAtIsNull(id)
+        .orElseThrow(() -> new RuntimeException());
+
+    ProductImageEntity productImage = ProductImageEntity.builder()
+        .originFileName(file.getOriginalFilename())
+        .imageUrl(fileName)
+        .fileSize(file.getSize())
+        .s3Folder(s3Folder)
+        .product(product)
+        .build();
+
+    productImageRepository.save(productImage);
+
   }
 }
