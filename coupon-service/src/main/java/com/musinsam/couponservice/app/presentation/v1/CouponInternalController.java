76f238a40372ce9e6094d04cab98a789b2d41a15
LimdaeIl <optional94@naver.com>
2025-04-19 12:48:55 +0900

    feat #190: 서비스 구현 및 컨트롤러 줄 수정

--- coupon-service/src/main/java/com/musinsam/couponservice/app/presentation/v1/CouponInternalController.java
+++ coupon-service/src/main/java/com/musinsam/couponservice/app/presentation/v1/CouponInternalController.java
@@ -10,7 +10,6 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,4 +33,6 @@ public class CouponInternalController {
         response
     ));
   }
+
+
 }
