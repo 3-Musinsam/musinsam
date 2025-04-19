58bab790d3b08ff3b0ecc2cb98d3f3fb13b1f30b
LimdaeIl <optional94@naver.com>
2025-04-19 18:37:55 +0900

    feat #194: 쿠폰 도메인 변경에 따른 인프라, 컨트롤러 수정
    querydsl 을 통해서 이미 발급 받은 이력이 있는지 확인한다.

--- coupon-service/src/main/java/com/musinsam/couponservice/app/domain/repository/coupon/CouponRepository.java
+++ coupon-service/src/main/java/com/musinsam/couponservice/app/domain/repository/coupon/CouponRepository.java
@@ -1,6 +1,7 @@
 package com.musinsam.couponservice.app.domain.repository.coupon;
 
 import com.musinsam.couponservice.app.domain.entity.coupon.CouponEntity;
+import com.musinsam.couponservice.app.domain.entity.couponPolicy.CouponPolicyEntity;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -13,4 +14,5 @@ public interface CouponRepository {
   Boolean existsCouponByCouponCode(String couponCode);
   List<CouponEntity> findAllByCouponPolicyEntity_CompanyId(UUID companyId);
   List<CouponEntity> findValidCouponsByUserId(Long userId);
+//  boolean existsByUserIdAndCouponPolicyId(Long userId, UUID couponPolicyId); // querydsl 로 다시 구현
 }
--- coupon-service/src/main/java/com/musinsam/couponservice/app/infrastructure/persistence/jpa/coupon/CouponJpaRepository.java
+++ coupon-service/src/main/java/com/musinsam/couponservice/app/infrastructure/persistence/jpa/coupon/CouponJpaRepository.java
@@ -2,7 +2,8 @@ package com.musinsam.couponservice.app.infrastructure.persistence.jpa.coupon;
 
 import com.musinsam.couponservice.app.domain.entity.coupon.CouponEntity;
 import com.musinsam.couponservice.app.domain.repository.coupon.CouponRepository;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CouponJpaRepository extends JpaRepository<CouponEntity, Long>, CouponRepository {
+public interface CouponJpaRepository extends JpaRepository<CouponEntity, UUID>, CouponRepository {
 }
