5f444f4c725e518ff548ffcc09b3ae3c61740c22
adgjl <adgjl0122@naver.com>
2025-04-22 21:11:56 +0900

    feat #224: Slack 알림봇 1:1 DM 전송 서비스 구현
    - Internal 구조 수정

--- alarm-service/src/main/java/com/musinsam/alarmservice/presentation/controller/AlarmInternalControllerApiV1.java
+++ alarm-service/src/main/java/com/musinsam/alarmservice/presentation/controller/AlarmInternalControllerApiV1.java
@@ -4,9 +4,7 @@ import com.musinsam.alarmservice.application.dto.request.ReqAlarmPostDtoApiV1;
 import com.musinsam.alarmservice.application.dto.response.ResAlarmGetByIdDtoApiV1;
 import com.musinsam.alarmservice.application.dto.response.ResAlarmGetDtoApiV1;
 import com.musinsam.alarmservice.application.dto.response.ResAlarmPostDtoApiV1;
-import com.musinsam.alarmservice.application.service.AlarmServiceApiV1;
-import com.musinsam.common.resolver.CurrentUser;
-import com.musinsam.common.user.CurrentUserDtoApiV1;
+import com.musinsam.alarmservice.application.service.AlarmInternalServiceApiV1;
 import jakarta.validation.Valid;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -26,20 +24,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/internal/v1/slack-alarms")
 public class AlarmInternalControllerApiV1 {
 
-  private final AlarmServiceApiV1 alarmServiceApiV1;
+  private final AlarmInternalServiceApiV1 alarmInternalServiceApiV1;
 
   @PostMapping
   public ResponseEntity<ResAlarmPostDtoApiV1> postBy(@Valid @RequestBody ReqAlarmPostDtoApiV1 dto) {
-    return ResponseEntity.ok(alarmServiceApiV1.postBy(dto));
+    return ResponseEntity.ok(alarmInternalServiceApiV1.postBy(dto));
   }
 
   @GetMapping
   public ResponseEntity<ResAlarmGetDtoApiV1> getBy(@Valid Pageable pageable) {
-    return ResponseEntity.ok(alarmServiceApiV1.getBy(pageable));
+    return ResponseEntity.ok(alarmInternalServiceApiV1.getBy(pageable));
   }
 
   @GetMapping("/{id}")
   public ResponseEntity<ResAlarmGetByIdDtoApiV1> getById(@Valid @PathVariable UUID id) {
-    return ResponseEntity.ok(alarmServiceApiV1.getById(id));
+    return ResponseEntity.ok(alarmInternalServiceApiV1.getById(id));
   }
 }
