a2da1ccf5915f90facb3249468a3d8fb3f032e3e
aerhergag00 <jwlee101500@gmail.com>
2025-04-22 14:52:18 +0900

    feat: K6, prometheus, grafana 설정

---
+++ infrastructure/k6-scripts/order-api-stress-test.js
@@ -0,0 +1,64 @@
+import http from 'k6/http';
+
+const token = __ENV.TEST_AUTH_TOKEN;
+const userId = __ENV.TEST_USER_ID;
+const userRole = __ENV.TEST_USER_ROLE;
+
+export const options = {
+  scenarios: {
+    ramping_test: {
+      executor: 'per-vu-iterations',
+      vus: 1000,
+      iterations: 10,
+      startTime: '0s',
+      maxDuration: '1m',
+    },
+  },
+};
+
+export default function () {
+  const url = 'http://host.docker.internal:10000/v1/orders';
+
+  const payload = JSON.stringify({
+    order: {
+      orderItems: [
+        {
+          id: 'f65a955a-abcd-4b5c-9cf4-149bd662e783',
+          productName: 'Sample Product - A',
+          price: 150000,
+          quantity: 3,
+        },
+        {
+          id: '3f6164db-5907-4cf0-afe4-c219527516ae',
+          productName: 'Sample Product - B',
+          price: 10000,
+          quantity: 5,
+        },
+      ],
+      shippingInfo: {
+        receiverName: 'John Doe',
+        receiverPhone: '010-1234-5678',
+        zipCode: '12345',
+        address: 'Shipping Address',
+        addressDetail: 'Shipping Address Detail',
+      },
+      request: 'Request Example',
+      orderName: 'Order Name Sample',
+      couponId: 'e676a7e8-9ab4-4452-a41c-351b69bba2d1',
+      totalAmount: 500000,
+      discountAmount: 33333,
+      finalAmount: 466667,
+    },
+  });
+
+  const params = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+      'X-User-Id': userId,
+      'X-User-Role': userRole
+    },
+  };
+
+  http.post(url, payload, params);
+}
