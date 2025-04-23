079df7a4da8870792a375f01330a894962c5a429
LimdaeIl <optional94@naver.com>
2025-04-23 12:52:29 +0900

    feat #229: 쿠폰 컨트롤러 구현 V4

---
+++ coupon-service/src/main/java/com/musinsam/couponservice/app/presentation/v4/CouponInternalController.java
@@ -0,0 +1,35 @@
+package com.musinsam.couponservice.app.presentation.v4;
+
+
+import com.musinsam.common.resolver.CurrentUser;
+import com.musinsam.common.user.CurrentUserDtoApiV1;
+import com.musinsam.couponservice.app.application.dto.v3.coupon.request.ReqCouponUseDtoApiV3;
+import com.musinsam.couponservice.app.application.dto.v3.coupon.response.ResCouponUseDtoApiV3;
+import com.musinsam.couponservice.app.application.service.v4.CouponService;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/internal/v4/coupons")
+@RestController("couponInternalControllerV4")
+public class CouponInternalController {
+
+  private final CouponService couponService;
+
+  @PostMapping("/{couponId}/use")
+  public ResponseEntity<ResCouponUseDtoApiV3> useCoupon(
+      @PathVariable UUID couponId,
+      @RequestBody ReqCouponUseDtoApiV3 request,
+      @CurrentUser CurrentUserDtoApiV1 currentUser
+  ) {
+    ResCouponUseDtoApiV3 response = couponService.useCoupon(couponId, request, currentUser);
+
+    return ResponseEntity.ok(response);
+  }
+}
